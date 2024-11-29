204e5bdb5b405cb5f8717249ca3d67c26134d2e8
halangbacca <halanbacca@outlook.com>
2024-11-29 18:29:20 -0300

    Instalação do pacote UI Vue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,4 +17,22 @@ class AuthController extends Controller
             return response()->json(['error' => 'Unauthorized'], 403);
         }
     }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+
+    public function refresh()
+    {
+        $token = auth('api')->refresh();
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 }
