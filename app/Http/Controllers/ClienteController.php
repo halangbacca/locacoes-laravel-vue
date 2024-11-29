af102e4c769bb25956675080abb24aa7ada184a0
halangbacca <halanbacca@outlook.com>
2024-11-29 12:19:57 -0300

    Implementação do ClienteController

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,64 +2,120 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreClienteRequest;
-use App\Http\Requests\UpdateClienteRequest;
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
+use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    public function __construct(Cliente $cliente)
+    {
+        $this->cliente = $cliente;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+
+        if ($request->has('filtro')) {
+            $clienteRepository->filtro($request->input('filtro'));
+        }
+
+        if ($request->has('atributos')) {
+            $clienteRepository->selectAtributos($request->input('atributos'));
+        }
+
+        return response()->json($clienteRepository->getResultado(), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \App\Http\Requests\StoreClienteRequest $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreClienteRequest $request)
+    public function store(Request $request)
     {
+        $request->validate($this->cliente->rules());
 
+        $cliente = $this->cliente->create([
+            'nome' => $request->input('nome'),
+        ]);
+
+        return response()->json($cliente, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param \App\Models\Cliente $cliente
-     * @return \Illuminate\Http\Response
+     * @param Integer
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if ($cliente === null) {
+            return response()->json(['erro' => 'Nenhum registro encontrado.'], 404);
+        }
+
+        return response()->json($cliente, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param \App\Http\Requests\UpdateClienteRequest $request
-     * @param \App\Models\Cliente $cliente
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Integer
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if ($cliente === null) {
+            return response()->json(['erro' => 'Nenhum registro encontrado.'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            foreach ($cliente->rules() as $input => $rule) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $rule;
+                }
+            }
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($cliente->rules());
+        }
+
+        $cliente->fill($request->all());
+        $cliente->save();
+
+        return response()->json($cliente, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param \App\Models\Cliente $cliente
-     * @return \Illuminate\Http\Response
+     * @param Integer
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if ($cliente === null) {
+            return response()->json(['erro' => 'Nenhum registro encontrado.'], 404);
+        }
+
+        $cliente->delete();
+        return response()->json('Registro removido com sucesso.', 204);
     }
 }
